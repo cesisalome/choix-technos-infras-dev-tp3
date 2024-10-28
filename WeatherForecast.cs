23b9ba92c21a0d1b6e2b4729e6316ba7926bdf18
Salomé <salome.cliquennois@viacesi.fr>
2024-10-28 16:33:59 +0100

    init

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Choix_des_technos_et_infras_de_développement___TP3
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
